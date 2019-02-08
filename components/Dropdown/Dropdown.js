eb4ba33e6605bc2df20a2bc287f60a44a104b215
Edgar F <eflores.ef@gmail.com>
2019-02-07 19:09:10 -0800

    Finished Dropdown component

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,6 +1,6 @@
 class Dropdown {
   constructor(element) {
-    
+
     // Assign this.element to the dropdown element
     this.element = element;
     
@@ -8,14 +8,11 @@ class Dropdown {
     this.button = this.element.querySelector('.dropdown-button');
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content = this.button.querySelector('.dropdown-content');
+    this.content = this.element.querySelector('.dropdown-content');
     
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', () => {
-      this.content.toggleContent();
-    })
+    this.button.addEventListener('click', () => this.toggleContent());
   }
-
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
